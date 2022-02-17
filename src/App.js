0a8f0531bd2f2235459a00e0325e5195ecab666d
Senad Zenkovic <zenkovic88@gmail.com>
2022-02-17 14:55:27 -0700

    add  new route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/brewerier-capstone" element={<Home />} />
+
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="login" element={<Login />} />
